c64d66dd14a1122cd5b6c1003b18cd7182f1ccca
IvovdBruggen <vanderbruggenivo@gmail.com>
2019-03-21 21:11:25 +0100

    added next race function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+var $ = jQuery = require('jQuery')(window);
+var token = require('./token.json');
 
 const trigger = ';';
 
 try {
-    client.login('NDUxODE5NDk2MjYxMjg3OTM3.D3UYDw.hJGv2nRxWau4OH_grFGoFboZwLM');
+    client.login(token.secret);
 } catch(err) {
     console.log(err);
 }
@@ -13,23 +20,43 @@ client.on('ready', () => {
     console.log('Logged in as ' + client.user.tag + '!');
 });
 
-
 client.on('message', msg => {
     if(msg.content.startsWith(trigger)) {
         $.getJSON(
             "http://ergast.com/api/f1/current/next.json",
             (data) => {
                 if(msg.content === ';next race') {
-                    
+                    msg.channel.send(
+                        {
+                            "embed": {
+                              "color": 3447003,
+                              "footer": {
+                                "text": "F1-Bot by Ivo van der Bruggen"
+                              },
+                              /*"author": {
+                                "name": "F1-Bot"
+                              },*/
+                              "fields": [
+                                {
+                                  "name": "Next race",
+                                  "value": "Next race will take place in: " + data.MRData.RaceTable.Races[0].Circuit.Location.country,
+                                  "inline": true
+                                },
+                                {
+                                  "name": "Time",
+                                  "value": "The race will take place at: " + data.MRData.RaceTable.Races[0].time,
+                                  "inline": true
+                                },
+                                {
+                                  "name": "Round",
+                                  "value": "It's gonna be round number " + data.MRData.RaceTable.Races[0].round + " of the F1 World Championship 2019!"
+                                }
+                              ]
+                            }
+                        }
+                    );
                 }
             }
         );
-    }
-});
-
-
-client.on('message', msg => {
-    if(msg.content === 'ping') {
-        msg.reply('pong');
     }
 });
